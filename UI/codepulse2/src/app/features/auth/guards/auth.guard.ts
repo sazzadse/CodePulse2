94b116a92196d939e780db69940be041e948c88f
Md. Sazzad Hossain <sazzad.se@gmail.com>
2025-02-18 18:06:37 +0600

    jwt decode up

--- UI/codepulse2/src/app/features/auth/guards/auth.guard.ts
+++ UI/codepulse2/src/app/features/auth/guards/auth.guard.ts
@@ -1,5 +1,22 @@
-import { CanActivateFn } from '@angular/router';
+// import { inject } from '@angular/core';
+// import { CanActivateFn, Router } from '@angular/router';
+// import { CookieService } from 'ngx-cookie-service';
+// import { AuthService } from '../services/auth.service';
+// import jwt_decode from 'jwt_decode';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+// export const authGuard: CanActivateFn = (route, state) => {
+//   const cookieService = inject(CookieService);
+//   const authService = inject(AuthService);
+//   const router = inject(Router);
+  
+//   // Check for the JWT Token
+//   let token = cookieService.get('Authorization');
+
+//   if (token) {
+//     token = jwt_decode(token);
+//   } else {
+//     // Logout 
+//     authService.logout();
+//     return router.createUrlTree(['/login'], { queryParams : { returnUrl: state.url }})
+//   }
+// };

3168b280d400f0288c5ddc9476c4d400c2cfe9c6
Sazzad Hossain <sazzad.se@gmail.com>
2025-02-21 22:17:29 +0600

    update after completed the tutorial

--- UI/codepulse2/src/app/features/auth/guards/auth.guard.ts
+++ UI/codepulse2/src/app/features/auth/guards/auth.guard.ts
@@ -1,22 +1,46 @@
-// import { inject } from '@angular/core';
-// import { CanActivateFn, Router } from '@angular/router';
-// import { CookieService } from 'ngx-cookie-service';
-// import { AuthService } from '../services/auth.service';
-// import jwt_decode from 'jwt_decode';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from '../services/auth.service';
+import { jwtDecode } from 'jwt-decode';
 
-// export const authGuard: CanActivateFn = (route, state) => {
-//   const cookieService = inject(CookieService);
-//   const authService = inject(AuthService);
-//   const router = inject(Router);
-  
-//   // Check for the JWT Token
-//   let token = cookieService.get('Authorization');
+export const authGuard: CanActivateFn = (route, state) => {
+  const cookieService = inject(CookieService);
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const user = authService.getUser();
 
-//   if (token) {
-//     token = jwt_decode(token);
-//   } else {
-//     // Logout 
-//     authService.logout();
-//     return router.createUrlTree(['/login'], { queryParams : { returnUrl: state.url }})
-//   }
-// };
+  // Check for the JWT Token
+  let token = cookieService.get('Authorization');
+
+  if (token && user) {
+    token = token.replace('Bearer ', '');
+    const decodedToken: any = jwtDecode(token);
+
+    // Check if token has expired
+    const expirationDate = decodedToken.exp * 1000;
+    const currentTime = new Date().getTime();
+
+    if (expirationDate < currentTime) {
+      // Logout
+      authService.logout();
+      return router.createUrlTree(['/login'], {
+        queryParams: { returnUrl: state.url },
+      });
+    } else {
+      // Token is still valid
+      if (user.roles.includes('Writer')) {
+        return true;
+      } else {
+        alert('Unauthorized');
+        return false;
+      }
+    }
+  } else {
+    // Logout
+    authService.logout();
+    return router.createUrlTree(['/login'], {
+      queryParams: { returnUrl: state.url },
+    });
+  }
+};
